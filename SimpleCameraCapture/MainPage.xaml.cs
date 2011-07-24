c8bddd6a0b1c10ee83a1ad1d72ed0b9858d17f62
Jeff Vera <jeff.vera@gmail.com>
2011-07-24 19:35:24 -0400

    Final cut.

--- SimpleCameraCapture/MainPage.xaml.cs
+++ SimpleCameraCapture/MainPage.xaml.cs
@@ -10,20 +10,43 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace SimpleCameraCapture
 {
 	public partial class MainPage : PhoneApplicationPage
 	{
-		// Constructor
+		CameraCaptureTask camera = new CameraCaptureTask();
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+			camera.Completed += new EventHandler<PhotoResult>(camera_Completed);
+		}
+
+		void camera_Completed(object sender, PhotoResult e)
+		{
+			switch (e.TaskResult)
+			{
+				case TaskResult.OK:
+					BitmapImage bmp = new BitmapImage();
+					bmp.SetSource(e.ChosenPhoto);
+					takenPicture.Source = bmp;
+					break;
+				case TaskResult.Cancel:
+					MessageBox.Show("User cancelled picture taking.");
+					break;
+				case TaskResult.None:
+					MessageBox.Show("Picture capture unsuccessful.");
+					break;
+			}
 		}
 
 		private void takeThePic_Click(object sender, RoutedEventArgs e)
 		{
-
+			camera.Show();
 		}
 	}
 }
